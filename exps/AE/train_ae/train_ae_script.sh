027482b4455af03012ee9e08e84718c8fe5ec885
Gaëtan Serré <gaetan.serre@universite-paris-saclay.fr>
2022-07-28 17:09:53 +0200

    New experiments

--- exps/AE/train_ae/train_ae_script.sh
+++ exps/AE/train_ae/train_ae_script.sh
@@ -4,7 +4,7 @@ python train_ae.py \
 --dataset dataset_516252obs.npz \
 --nb_epochs 60 \
 --latent_dim 400 \
---arch_enc 1024 800 600 \
---arch_dec 600 800 1024 \
+--arch_enc 600 \
+--arch_dec 600 \
 --batch_size 1024 \
 --nb_models 10
