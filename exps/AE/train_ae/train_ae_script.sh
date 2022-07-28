8e2bde5c01e47a1db15b5fe2514825c80de91e01
Gaëtan Serré <gaetan.serre@universite-paris-saclay.fr>
2022-07-28 12:19:39 +0200

    Update train_ae_script.sh

--- exps/AE/train_ae/train_ae_script.sh
+++ exps/AE/train_ae/train_ae_script.sh
@@ -1,10 +1,10 @@
 set -x
 
 python train_ae.py \
---dataset dataset_254397obs.npz \
---nb_epochs 5 \
+--dataset dataset_516252obs.npz \
+--nb_epochs 60 \
 --latent_dim 400 \
 --arch_enc 1024 800 600 \
 --arch_dec 600 800 1024 \
---batch_size 512 \
---nb_models 3
+--batch_size 1024 \
+--nb_models 10
